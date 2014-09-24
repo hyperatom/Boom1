a2e32a8de8b1515d30158c971a99c67704f73ffa
B8959834_Adam_Barrell <b8959834@MB506.local>
2014-09-24 14:14:43 +0100

    Implemented initial plp page with dummy data.

--- app/bower_components/ionic/release/js/ionic.bundle.js
+++ app/bower_components/ionic/release/js/ionic.bundle.js
@@ -27588,12 +27588,12 @@ var ngNonBindableDirective = ngDirective({ terminal: true, priority: 1000 });
  * These rules are bundled with angular.js, but can be overridden
  * (see {@link guide/i18n Angular i18n} dev guide). You configure ngPluralize directive
  * by specifying the mappings between
- * [plural categories](http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html)
+ * [plural categories](http://unicode.org/repositories/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html)
  * and the strings to be displayed.
  *
  * # Plural categories and explicit number rules
  * There are two
- * [plural categories](http://unicode.org/repos/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html)
+ * [plural categories](http://unicode.org/repositories/cldr-tmp/trunk/diff/supplemental/language_plural_rules.html)
  * in Angular's default en-US locale: "one" and "other".
  *
  * While a plural category may match many numbers (for example, in en-US locale, "other" can match
